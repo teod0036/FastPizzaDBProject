fb80c03a39e61016bff5d2d0c8ff4fe17847cc11
MartinKeithBuchreitz <Martinbu29@gmail.com>
2022-10-30 16:37:34 +0100

    Prototype Interface
    ds

--- FastPizzaProject/Program.cs
+++ FastPizzaProject/Program.cs
@@ -10,6 +10,39 @@ namespace FastPizzaProject
     {
         static void Main(string[] args)
         {
+            char input;
+
+            Console.WriteLine("Hello and Welcome to Fast Pizza");
+            Console.WriteLine("First choose which restuarant you want to order from:");
+            for (int i = 1; i < args.Length; i++) // writing the resturants
+            {
+                Console.WriteLine(i + ":" + i);
+            }        
+
+            input = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\n" + input);
+
+            if (input == 0) // check if the user input matches with a resturant 
+            {
+
+            } 
+
+                Console.WriteLine("Choose which item you want to order:");
+            for (int i = 1; i < args.Length; i++) // writing the menu
+            {
+
+            }
+            input = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\n" + input);
+
+            Console.WriteLine("you have ordered " + input);
+            Console.WriteLine("Do you want to confirm your order?");
+            Console.WriteLine("1. Yes \n 2. No");
+            Console.ReadKey();
+
+
         }
     }
 }
